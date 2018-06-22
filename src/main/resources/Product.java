ef0665b75b46040757581fe544b3415e727b981b
frup70804 <frup70804@GFI520414.annuaire.groupe.local>
2018-06-22 17:45:10 +0200

    creation of Category and Product pre-bean

---
+++ src/main/resources/Product.java
@@ -0,0 +1,25 @@
+
+public class Product {
+
+	private String name;
+	private String website;
+	private String Brand;
+	private Date collectDate;
+
+	// Ranking x. y. Give me more information
+	// block(x/y)
+	private int Column;
+	private int Line;
+
+	// Category?
+	private String model;
+	private String size;
+
+	// price more complex?
+	private double price;
+	private String currency;
+	
+	// basic color
+	private String color;
+	// complex color: "deep precious dark and some point of green"
+}
